019d693f89951ade10d4594f38012a936701eaef
DianoEdriel <92987713+DianoEdriel@users.noreply.github.com>
2023-06-25 16:10:31 +0800

    .

---
+++ Dashboard.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <?php
+            include 'include/head.html';
+        ?>
+</head>
+
+<body>
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-2 border border-danger">
+                <div class= "container border border-primary" style = "height:100pximportant;">
+                    1
+                </div>
+            </div>
+            <div class="col-sm-10 border border-success">
+                2
+            </div>
+        </div>
+    </div>
+
+
+</body>
+
+</html>

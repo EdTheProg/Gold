440bc03e33dbb9d1f78f2485cefc2e483c19f536
EdTheProg <131273109+EdTheProg@users.noreply.github.com>
2024-01-03 22:31:38 +0800

    timeoff/on
    time on and off with some modification on sangla

---
+++ php/Sangla/view/newSangla.php
@@ -0,0 +1,33 @@
+<?php
+spl_autoload_register(function ($class) {
+  include '../../../classes/' . $class . '.php';
+});
+
+use Controller\Login;
+
+
+echo "test";
+
+switch ($_SERVER['REQUEST_METHOD']) {
+  case 'POST':
+    echo "method is post";
+    break;
+  case 'GET':
+    echo "method is GET";
+    break;
+  default:
+    http_response_code(405);
+    break;
+}
+  /*
+  if (isset($_GET)){
+
+     echo "tesss";
+    
+  }
+  else{
+    http_response_code(400);
+  }
+  */
+  
+?>
